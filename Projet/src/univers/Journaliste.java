bd6a42e317fcb3df38a3179bc372bfa8f7f6e152
BabiscoJr <153921463+BabiscoJr@users.noreply.github.com>
2023-12-18 16:36:37 +0100

    Update Journaliste.java

--- Projet/src/univers/Journaliste.java
+++ Projet/src/univers/Journaliste.java
@@ -4,19 +4,22 @@ import java.util.ArrayList;
 
 public class Journaliste extends personnage_de_base {
 
-		public Journaliste() {
-			super();
-		}
-		
-		public Journaliste(int a, int b, int c, ArrayList<String> d) {
-			super(a,b,c,d);
-		}
-		
-		public String toString() {
-			return ("Le personnage est un journaliste.\n"+super.toString());
-		}
-		
-		public boolean equals(Object o) {
-			return super.equals(o);
-		}
+    public Journaliste() {
+        super();
+    }
+
+    public Journaliste(String nom, int niveau, int a, int b, int c, ArrayList<String> d,
+                       int statsCombat, int statsNegociation, int statsFuite) {
+        super(nom, niveau, a, b, c, d, statsCombat, statsNegociation, statsFuite);
+    }
+
+    @Override
+    public String toString() {
+        return "Le personnage est un journaliste.\n" + super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
 }
