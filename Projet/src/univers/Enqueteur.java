c5f25267285c6c781265e34110932530a9266c93
BabiscoJr <153921463+BabiscoJr@users.noreply.github.com>
2023-12-18 16:33:10 +0100

    Update Enqueteur.java

--- Projet/src/univers/Enqueteur.java
+++ Projet/src/univers/Enqueteur.java
@@ -1,22 +1,19 @@
+
 package univers;
 
 import java.util.ArrayList;
 
 public class Enqueteur extends personnage_de_base {
 
-		public Enqueteur() {
-			super();
-		}
-		
-		public Enqueteur(int a, int b, int c, ArrayList<String> d) {
-			super(a,b,c,d);
-		}
-		
-		public String toString() {
-			return ("Le personnage est un enquêteur.\n"+super.toString());
-		}
-		
-		public boolean equals(Object o) {
-			return super.equals(o);
-		}
+    public Enqueteur() {
+        super();
+    }
+
+    public Enqueteur(String nom, int niveau, int intelligence, int physique, int contacts, ArrayList<String> outils,
+                     int statsCombat, int statsNegociation, int statsFuite) {
+        super(nom, niveau, intelligence, physique, contacts, outils, statsCombat, statsNegociation, statsFuite);
+    }
+
+    
 }
+
