2da5dd9c2614358b7a21ebe9a905207709a4dde4
BabiscoJr <153921463+BabiscoJr@users.noreply.github.com>
2023-12-18 16:38:30 +0100

    Update Espion.java

--- Projet/src/univers/Espion.java
+++ Projet/src/univers/Espion.java
@@ -1,22 +1,27 @@
 package univers;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Espion extends personnage_de_base {
-		
-		public Espion() {
-			super();
-		}
-		
-		public Espion(int a, int b, int c, ArrayList<String> d) {
-			super(a,b,c,d);
-		}
-		
-		public String toString() {
-			return ("Le personnage est un espion.\n"+super.toString());
-		}
-		
-		public boolean equals(Object o) {
-			return super.equals(o);
-		}
+
+    public Espion() {
+        super();
+    }
+
+    public Espion(String nom, int niveau, int a, int b, int c, ArrayList<String> d,
+                  int statsCombat, int statsNegociation, int statsFuite) {
+        super(nom, niveau, a, b, c, d, statsCombat, statsNegociation, statsFuite);
+    }
+
+    @Override
+    public String toString() {
+        return "Le personnage est un espion.\n" + super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
 }
